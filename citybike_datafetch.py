3f76c7521f38a67b1183683f3d6b2d7b6a3ead5a
Avncoolspirit <kumaravnish91@gmail.com>
2020-02-23 20:58:39 -0500

    changed vars and added comments

--- citybike_datafetch.py
+++ citybike_datafetch.py
@@ -3,25 +3,28 @@ import pymysql
 import json
 import time
 
+#TO DO: Make the script to make a schema in the rds database
 
+#make the connection with the datbase
 db = pymysql.connect("xxxx.abcd.com","user","password","db" )
 
 
-response = urllib2.urlopen('https://feeds.citibikenyc.com/stations/stations.json')
-html = json.loads(response.read())
+#read data from the opennyc citibike feed
+response_citibike = urllib2.urlopen('https://feeds.citibikenyc.com/stations/stations.json')
+html = json.loads(response_citibike.read())
 
-response1 = urllib2.urlopen('https://api.darksky.net/forecast/ed1a586e14484a2dda7a4e8fa3b47fd8/40.7128,-74.0060')
-#html1 = json.loads(response.read())
-new_weather = response1.read()
+response_weather = urllib2.urlopen('https://api.darksky.net/forecast/ed1a586e14484a2dda7a4e8fa3b47fd8/40.7128,-74.0060')
+new_weather = response_weather.read()
 weather = str(eval(new_weather)['currently'])
 
+#parse the data and split it according to the station
 data_database=[]
 for station in html['stationBeanList']:
     data_database.append((time.time(),station['stationName'],str(station),weather))
 
+#feed into the database
 query="""insert into gurlam_3456( timestamps, station, data, weather) values (%s, %s, %s, %s)"""
 cursor  = db.cursor()
-#print data_database
 cursor.executemany(query, data_database)
 db.commit()
 
